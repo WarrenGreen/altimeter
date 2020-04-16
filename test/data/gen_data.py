6e2b0a44c1ef4677e3261f4d2667b179023691b7
wgreen <wgreen@twilio.com>
2020-04-15 18:06:59 -0700

    Add complex polynomial altitude generator

--- test/data/gen_data.py
+++ test/data/gen_data.py
@@ -1,8 +1,11 @@
 import json
 import random
-from math import sin
+from math import sin, cos
+
+import numpy as np
 
 from altimeter.barometer import Barometer
+from altimeter.smoothing import smooth
 
 
 def sin_gen():
@@ -13,6 +16,41 @@ def sin_gen():
 
 def linear_gen(altitude_start=1524, altitude_end=2134):
     for altitude in range(altitude_start, altitude_end, 1):
+        yield altitude
+
+
+def generate_polynomial(max_poly_size=4):
+    poly_size = random.randint(1, max_poly_size)
+
+    def poly(x):
+        poly_value = 0
+        for poly_index in range(2, poly_size + 1):
+            if poly_index % 2 == 0:
+                poly_value += pow(sin(x), poly_index)
+            else:
+                poly_value += pow(cos(x), poly_index)
+
+        slope_dir = 1 if random.random() > 0.5 else -1
+        poly_value *= slope_dir
+        return poly_value
+
+    return poly
+
+
+def poly_gen(n=500):
+    poly = generate_polynomial()
+    altitude = 0.0
+    altitudes = []
+    for index in range(n):
+        if index % 6:
+            poly = generate_polynomial()
+        time = index / 100.0
+        altitude_delta = poly(time)
+        altitude += altitude_delta
+        altitudes.append(altitude)
+
+    smoothed_altitudes = smooth(np.array(altitudes))
+    for altitude in smoothed_altitudes:
         yield altitude
 
 
@@ -31,8 +69,10 @@ def main(mode="linear"):
     barometer = Barometer()
     if mode == "linear":
         altitude_gen = linear_gen()
-    else:
+    elif mode == "sine":
         altitude_gen = sin_gen()
+    else:
+        altitude_gen = poly_gen()
     with open(output_filepath, "w") as f:
         for altitude in altitude_gen:
             pressure = barometer.pressure(altitude)
@@ -63,4 +103,4 @@ def main(mode="linear"):
 
 
 if __name__ == "__main__":
-    main("linear")
+    main("poly")
